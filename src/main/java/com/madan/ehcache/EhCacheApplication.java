933cd62f2e4ce98a7e2d8a915049453fb9e24dcd
Madan <mksiwakoti@gmail.com>
2018-03-31 12:47:44 -0500

    first commit

---
+++ src/main/java/com/madan/ehcache/EhCacheApplication.java
@@ -0,0 +1,12 @@
+package com.madan.ehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhCacheApplication.class, args);
+	}
+}
